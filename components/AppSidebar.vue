14f93f19e06b000ffe333495f1ba6432dd2e460b
harris <harris.kunakira@gmail.com>
2025-01-11 23:03:54 +0700

    add sidebar

---
+++ components/AppSidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ShadSidebarProvider :open="true">
+    <ShadSidebar>
+      <ShadSidebarHeader> Langasus </ShadSidebarHeader>
+      <ShadSidebarContent>
+        <ShadSidebarGroup>
+          <ShadSidebarMenu>
+            <ShadSidebarMenuItem>
+              <!-- <ShadSidebarMenuTrigger>Overview</ShadSidebarMenuTrigger> -->
+              Menu
+            </ShadSidebarMenuItem>
+          </ShadSidebarMenu>
+        </ShadSidebarGroup>
+      </ShadSidebarContent>
+    </ShadSidebar>
+  </ShadSidebarProvider>
+</template>
